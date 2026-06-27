7ed407b0e8830895ceaf5a3d9167d0fdbe9d00eb
Aayush Gupta <guptaaayush537@gmail.com>
2026-06-27 14:07:09 +0530

    ui changed with models options

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-   server: {
-     host: true,
-      port: 5173,
+  plugins: [react(), tailwindcss()],
+  server: {
+    host: true,
+    port: 5173,
     allowedHosts: [
       "gap-senator-ultra.ngrok-free.dev",
       "localhost",
       "http://192.168.20.76:5173/",
-    ]
-  }
-})
+    ],
+  },
+});
